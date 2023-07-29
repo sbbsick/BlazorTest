c1320918eb7ae8b34d5c5675569e467fe943858b
sbbsick <silasbramos@outlook.com>
2023-07-29 00:38:15 -0300

    Feito a o component de inserir um novo peixe.

--- BlazorTest/Data/Fish.cs
+++ BlazorTest/Data/Fish.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorTest.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTest.Data;
 
 public class Fish
 {
@@ -8,9 +10,17 @@ public class Fish
     }
 
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "This field is required.")]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "This field is required.")]
     public int Length { get; set; }
+
+    [Required(ErrorMessage = "This field is required.")]
     public float Weight { get; set; }
+
+    [Required(ErrorMessage = "This field is required.")]
     public EFishType Type { get; set; }
 
 }
