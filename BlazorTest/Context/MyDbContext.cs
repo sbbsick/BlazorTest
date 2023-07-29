57c65c3adc11ca2954cedb9ce5a188934ae3ba24
sbbsick <silasbramos@outlook.com>
2023-07-29 17:00:17 -0300

    Finalizado a lista de peixes do lázaro.

--- BlazorTest/Context/MyDbContext.cs
+++ BlazorTest/Context/MyDbContext.cs
@@ -12,11 +12,43 @@ public class MyDbContext : DbContext
 
     }
 
-    public void AddRange(IEnumerable<Fish> fishes)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        foreach (var fish in fishes)
+        modelBuilder.Entity<Fish>().HasKey(x => x.Id);
+        modelBuilder.Entity<Fish>().Property(x => x.Name).IsRequired();
+        modelBuilder.Entity<Fish>().Property(x => x.Length).IsRequired();
+        modelBuilder.Entity<Fish>().Property(x => x.Weight).IsRequired();
+        modelBuilder.Entity<Fish>().Property(x => x.Type).IsRequired();
+    }
+
+    public async Task SeedData()
+    {
+        var listFishes = new List<Fish>
         {
-            Fishes.Add(fish);
+            new Fish { Name = "Trout", Length = 10, Weight = 2.8f, Type = EFishType.Freshwater },
+            new Fish { Name = "Salmon", Length = 30, Weight = 10.8f, Type = EFishType.Saltwater },
+            new Fish { Name = "Piranha", Length = 14, Weight = 6.8f, Type = EFishType.Freshwater },
+            new Fish { Name = "Pacu", Length = 20, Weight = 12.8f, Type = EFishType.Freshwater },
+            new Fish { Name = "Salmon2", Length = 10, Weight = 2.8f, Type = EFishType.Saltwater },
+        };
+
+        foreach (var fish in listFishes)
+        {
+            if (!await Fishes.AnyAsync(x => x.Name == fish.Name))
+            {
+                await Fishes.AddAsync(fish);
+            }
+        }
+        await SaveChangesAsync();
+    }
+
+    public void DeleteFish(Guid id)
+    {
+        var fish = Fishes.FirstOrDefault(x => x.Id == id);
+        if (fish != null)
+        {
+            Fishes.Remove(fish);
         }
     }
+
 }
--- BlazorTest/Program.cs
+++ BlazorTest/Program.cs
@@ -1,5 +1,4 @@
 using BlazorTest.Context;
-using BlazorTest.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<MyDbContext>(options => options.UseInMemoryDatabase("DbTest"));
 
 var app = builder.Build();
